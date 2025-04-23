bc17e5d2bb9b4a39e856bfec8213889764892699
BibaBoba1337228 <andreyka.b1201@gmail.com>
2025-04-23 20:04:05 +0300

    font fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './styles/fonts.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import TaskDashboard from "./pages/TaskDashboard";
 import TaskCalendar from "./pages/TaskCalendar";
 import LeftMenuMessenger from "./components/LeftMenuMessenger";
 
+const router = createBrowserRouter([
+  {
+    element: <LeftMenuMessenger />,
+    children: [
+      {
+        index: true,
+        element: <TaskDashboard />,
+      },
+      {
+        path: "/calendar",
+        element: <TaskCalendar />,
+      },
+
+    ]
+  }
+]);
+
 function App() {
-  return (
-      <LeftMenuMessenger></LeftMenuMessenger>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
