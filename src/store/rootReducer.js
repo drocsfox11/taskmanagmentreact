64cf35866903bf0a428f694b771fa0e06735e2b9
drocsfox11 <drocsfox@gmail.com>
2025-05-01 17:51:09 +0300

    Реверт прав

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -7,7 +7,6 @@ import usersReducer from './features/users/usersSlice';
 import uiReducer from './features/ui/uiSlice';
 import boardsReducer from './features/boards/boardsSlice';
 import tagsReducer from './features/tags/tagsReducer';
-import permissionsReducer from './features/permissions/permissionsSlice';
 
 const rootReducer = combineReducers({
   tasks: tasksReducer,
@@ -18,7 +17,6 @@ const rootReducer = combineReducers({
   ui: uiReducer,
   boards: boardsReducer,
   tags: tagsReducer,
-  permissions: permissionsReducer,
 });
 
 export default rootReducer; 
